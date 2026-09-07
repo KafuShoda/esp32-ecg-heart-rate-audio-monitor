ce69d9cb72280aa3698cf1ef2351d97b323e58eb
Kafu Kristian Shoda <kafu.shoda@gmail.com>
2026-09-07 19:47:08 -0400

    Update i2c-scanner.ino
    Add OLED I2C scanner using A2 and A3

--- code/phase-2-oled-test/i2c-scanner.ino
+++ code/phase-2-oled-test/i2c-scanner.ino
@@ -1,26 +1,32 @@
 #include <Wire.h>
 
-void setup() {
-  Wire.begin();
-  Serial.begin(115200);
+const int OLED_SDA_PIN = A2;
+const int OLED_SCL_PIN = A3;
 
+void setup() {
+  Serial.begin(115200);
   delay(2000);
 
   Serial.println();
-  Serial.println("I2C Scanner");
-  Serial.println("Scanning...");
+  Serial.println("OLED I2C Scanner");
+  Serial.println("SDA/Data = A2");
+  Serial.println("SCL/Clock = A3");
+
+  Wire.begin(OLED_SDA_PIN, OLED_SCL_PIN);
+  Wire.setClock(100000);
 }
 
 void loop() {
-  byte error;
-  byte address;
-  int deviceCount = 0;
+  int devicesFound = 0;
 
-  for (address = 1; address < 127; address++) {
+  Serial.println();
+  Serial.println("Scanning for I2C devices...");
+
+  for (byte address = 1; address < 127; address++) {
     Wire.beginTransmission(address);
-    error = Wire.endTransmission();
+    byte result = Wire.endTransmission();
 
-    if (error == 0) {
+    if (result == 0) {
       Serial.print("I2C device found at address 0x");
 
       if (address < 16) {
@@ -28,16 +34,15 @@ void loop() {
       }
 
       Serial.println(address, HEX);
-      deviceCount++;
+      devicesFound++;
     }
   }
 
-  if (deviceCount == 0) {
+  if (devicesFound == 0) {
     Serial.println("No I2C devices found.");
   } else {
     Serial.println("Scan complete.");
   }
 
-  Serial.println();
   delay(5000);
 }
